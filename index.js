06264a6064e48e60641bac093e34c73c67c2a977
RYAN KIZITO <kizitoryan@gmail.com>
2024-04-19 11:34:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$("#b1").on("click", function(){
+    location.href = "linnabweru.htnl";
+});
+$("#b2").on("click", function(){
+    location.href = "linkazo.html";
+});
+$("#i").on("click",function(){
+    $("#i").stop(animation);
+});
+$(".d1").on("click",function(){
+    location.href = "linnabweru.html";
+});
+$(".d2").on("click",function(){
+    location.href = "linkazo.html";
+});
+
+// LINNET-NABWERU'S jQUERY STARTS HERE!
+
+// $("#mm").css("height","292px")
+// $("#mm").css("width","200px")
